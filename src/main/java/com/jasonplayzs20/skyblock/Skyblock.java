753e5575a87c630e2a68794126814e71c6e625d6
BuildTools <unconfigured@null.spigotmc.org>
2022-04-05 17:31:05 -0400

    init

---
+++ src/main/java/com/jasonplayzs20/skyblock/Skyblock.java
@@ -0,0 +1,17 @@
+package com.jasonplayzs20.skyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skyblock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        //Never gonna give you up
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
